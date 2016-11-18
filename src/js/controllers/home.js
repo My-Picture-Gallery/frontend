85e803c36059a57f9e53c964596a2459aacabe15
Alex Windle <alexwindle85@gmail.com>
2016-11-18 17:27:30 -0500

    WIP - AddController failing.

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,19 +1,19 @@
-import SERVER from '../server';
+import { SERVER } from '../server';
 
-function HomeController ($scope, $http) {
+function HomeController ($scope, $http, $state) {
   // $scope.pics = [];
 
 	  function init() {
 	  	console.log("Success: In the HomeController")
 	    
-	    // $http.get(SERVER).then((response) => {
-	    //   $scope.pics = response.data;
-	    // });
+	    $http.get(SERVER).then((response) => {
+	      console.log('Gallery data -->', response)
+	    });
 	  }
 
 	  init();
 
 }; 
 
-HomeController.$inject = ['$scope', '$http'];
+HomeController.$inject = ['$scope', '$http', '$state'];
 export { HomeController };
