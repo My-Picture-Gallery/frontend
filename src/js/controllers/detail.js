fac5fd7270e7c16dfe1f1d52077cf380f3568a36
Mallerie Shirley <mallerie@gmail.com>
2016-11-19 17:16:33 -0500

    Wip - Detail view working

--- src/js/controllers/detail.js
+++ src/js/controllers/detail.js
@@ -3,14 +3,15 @@ import { SERVER } from '../server';
 function DetailController ($scope, $http, $stateParams) {
   $scope.pic = {};
 
-	  function init() {
-	  	console.log("Success: In the DetailController");
+    function init() {
+      console.log("Success: In the DetailController");
       console.log("This is stateParams", $stateParams);
       
       $http.get(SERVER + "image/" + $stateParams.id).then((response) => {
-	      $scope.pic = response.data;
-        // console.log(url);
-        // console.log(response.data);
+        $scope.pic = response.data[0];
+        console.log("response.data", response.data);
+        console.log("This is stateParams", $stateParams);
+
 	    });
 	  }
 
